4b7157fc9c09dfb79ad4baf0642f48a8cdd98c16
luebby <karsten@statistix.org>
2023-01-30 09:25:29 +0100

    Update

--- Workshop.R
+++ Workshop.R
@@ -15,7 +15,7 @@ exams2html("Fragen/F1_SitzFOM.Rmd")
 # Ohne Antworten
 exams2html("Fragen/F1_SitzFOM.Rmd", solution = FALSE)
 # Als pdf
-exams2pdf("Fragen/F1_SitzFOM.Rmd")
+exams2pdf("Fragen/F1_SitzFOM.Rmd", template = "tex/fom")
 
 ## Beipiel 2. Wie 1, nur mit R
 exams2html("Fragen/F2_SitzFOM_R.Rmd")
@@ -25,9 +25,8 @@ exams2html("Fragen/F3_SitzFOM_Dynamik.Rmd")
 exams2html("Fragen/F3_SitzFOM_Dynamik.Rmd")
 exams2html("Fragen/F3_SitzFOM_Dynamik.Rmd")
 
-## Beispiel 4. Zahl als Lösung
-exams2html("Fragen/F4_Algebra.Rmd", mathjax = TRUE)
-exams2html("Fragen/F4_Algebra.Rmd", mathjax = TRUE)
+## Beispiel 4. Zahl als Lösung - 2 Versionen
+exams2html("Fragen/F4_Algebra.Rmd", n = 2, mathjax = TRUE)
 
 ## Beispiel 5. Text als Lösung
 exams2html("Fragen/F5_Modellierung.Rmd")
@@ -37,5 +36,23 @@ set.seed(1896)
 klausurfragen <- c("Fragen/F3_SitzFOM_Dynamik.Rmd",
                    sample(c("Fragen/F4_Algebra.Rmd", 
                             "Fragen/F5_Modellierung.Rmd"), 1))
+# Export nach Word
+exams2pandoc(klausurfragen, type = "docx", name = "Testklausur")
 # Export nach moodle
-exams2moodle(klausurfragen)
+exams2moodle(klausurfragen, name = "Testklausur")
+
+# Export nach pdf - mit Umwandlung deutsche Texte etc.
+formatierung <- {
+  to_latex <- make_exercise_transform_pandoc(to = "latex")
+  function(x) {
+    x <- to_latex(x)
+    x$question <- c(sprintf("Aufgabe "), 
+                    sprintf("\\emph{(%s %s)}:", x$metainfo$points, 
+                            ifelse(x$metainfo$points != 1, "Punkte", 
+                                   "Punkt")), x$question)
+    return(x)
+  }
+}
+exams2pdf(klausurfragen, name = "Testklausur",
+             template = "tex/fom.tex", transform = formatierung)
+
